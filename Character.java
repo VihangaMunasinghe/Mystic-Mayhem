f0640887a1e50e0e23d9dcbed4e77c88bd69cd15
Vihanga Munasinghe <149761186+VihangaMunasinghe@users.noreply.github.com>
2024-03-09 13:00:03 +0530

    Character.java file updated

---
+++ Character.java
@@ -0,0 +1,50 @@
+public class Character {
+    private String name, category;
+    private int price, currentValue, attack, defence, health, speed;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getCurrentValue() {
+        return currentValue;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void changeSpeed(int amount){
+
+    }
+    public void changeAttack(int amount){
+
+    }
+    public void changeDefence(int amount){
+
+    }
+    public void changeHealth(int amount){
+
+    }
+
+}
