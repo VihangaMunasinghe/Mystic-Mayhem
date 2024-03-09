104fe6c579a4b7a7437b9d0a2cc4a424f2f08a3f
Umesha Jayakody <157822757+UmeshaJayakody@users.noreply.github.com>
2024-03-09 22:56:43 +0530

    Create Equipment.java

---
+++ Equipment.java
@@ -0,0 +1,74 @@
+public class Equipment {
+    private String name;
+    private int price;
+    private int attack;
+    private int defense;
+    private int health;
+    private int speed;
+
+    public Equipment(String name) {
+        this.name = name;
+        if (name.equals("Chainmail")) {
+            this.price = 70;
+            this.attack = 0;
+            this.defense = 1;
+            this.health = 0;
+            this.speed = -1;
+        }
+        else if (name.equals("Excalibur")) {
+            this.price = 150;
+            this.attack = 2;
+            this.defense = 0;
+            this.health = 0;
+            this.speed = 0;
+        }
+        else if (name.equals("Regalia")) {
+            this.price = 105;
+            this.attack = 0;
+            this.defense = 1;
+            this.health = 0;
+            this.speed = 0;
+        }
+        else if (name.equals("Amulet")) {
+            this.price = 200;
+            this.attack = 1;
+            this.defense = -1;
+            this.health = 1;
+            this.speed = 1;
+        }
+        else if (name.equals("Fleece")) {
+            this.price = 150;
+            this.attack = 0;
+            this.defense = 2;
+            this.health = 1;
+            this.speed = -1;
+        }
+        else if (name.equals("Crystal")) {
+            this.price = 210;
+            this.attack = 2;
+            this.defense = 1;
+            this.health = -1;
+            this.speed = -1;
+        }
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
