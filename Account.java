680550bf404a30a02fe7d80e4ae7b8c3bcb94425
dasunR430 <appuhamiwadr.22@uom.lk>
2024-03-13 18:48:49 +0530

    Fixed some validations in Account class.

--- Account.java
+++ Account.java
@@ -2,20 +2,41 @@ import javax.xml.crypto.Data;
 import java.util.Scanner;
 public class Account {
     public static Player createAccount(){
+        String userName,name;
         Scanner scanner = new Scanner(System.in);
         System.out.println("*** Create New Account ***");
-        System.out.print("Enter the name: ");
-        String name = scanner.nextLine();
 
-        System.out.print("Enter the username: ");
-        String userName = scanner.nextLine();
-        if(Database.isUserNameAvailable(userName)){
-            int userId = Database.getNextUserId();
-            Player player = new Player(userId, name, userName);
-            player.saveNewUser();
-            return player;
+        while(true) {
+            System.out.print("Enter the name: ");
+            name = scanner.nextLine();
+
+            if(!name.equals(""))
+                break;
+            else
+                System.out.println("Name cannot be empty");
         }
-        return null;
+
+
+        while(true) {
+            while(true) {
+                System.out.print("Enter the username: ");
+                userName = scanner.nextLine();
+
+                if(!userName.equals(""))
+                    break;
+                else
+                    System.out.println("Username cannot be empty");
+            }
+            if (Database.isUserNameAvailable(userName)) {
+                int userId = Database.getNextUserId();
+                Player player = new Player(userId, name, userName);
+                player.saveNewUser();
+                return player;
+            }
+            else {
+                System.out.println("Username is not available. Enter new username.");
+            }
+        }
     }
 
     public static Player logIn(){
